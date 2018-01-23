1f13d80c1ed93c7d33225352f38d1aba4030b6b4
juanma <juanmasl86@gmail.com>
2018-01-23 20:15:09 +0100

    actualizacion insercion

--- PutosAmos/src/Ordenacion.java
+++ PutosAmos/src/Ordenacion.java
@@ -1,5 +1,38 @@
 
 public class Ordenacion {
+	
+			// variables//
+
+			private int numero, i, j, temp;
+
+			// vector//
+
+			private int[] vector;
+			vector = new private int[10];
+	
+	public static void InsercionAscendente (vector int[]) {
+		for (i = 0; i < vector.length; i++) {
+			temp = vector[i];
+			j = i - 1;
+			while (j >= 0 && vector[j] > temp) {
+				vector[j + 1] = vector[j];
+				vector[j] = temp;
+				j--;
+			}
+		}
+	}
+	
+	public static void InsercionDescendente (vector int[]) {
+		for (i = 0; i < vector.length; i++) {
+			temp = vector[i];
+			j = i - 1;
+			while (j >= 0 && vector[j] < temp) {
+				vector[j + 1] = vector[j];
+				vector[j] = temp;
+				j--;
+			}
+		}
+	}
 
 	
 }
