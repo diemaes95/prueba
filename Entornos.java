6a7289c528793530d00ce9feb4bfe4af917244a4
Iván <ilopper.pio12@gmail.com>
2018-01-23 20:16:03 +0100

    modifico entornos

--- Entornos.java
+++ Entornos.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Entornos {
 
-	public static void insercion(int[] insercion) {
+	public static void insercion(int[] array) {
 
 		// Variables.
 
@@ -12,13 +12,13 @@ public class Entornos {
 
 		// Ordenamos el array mediante el método de inserción (crecientemente).
 
-		for (i = 0; i < insercion.length; i++) {
-			aux = insercion[i];
+		for (i = 0; i < array.length; i++) {
+			aux = array[i];
 			j = i - 1;
 
-			while (j >= 0 && insercion[j] > aux) {
-				insercion[j + 1] = insercion[j];
-				insercion[j] = aux;
+			while (j >= 0 && array[j] > aux) {
+				array[j + 1] = array[j];
+				array[j] = aux;
 				j--;
 			}
 		}
