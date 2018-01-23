93251a89728e585be50195f2ded6d38c438053ae
DanielGC <hola@hola.com>
2018-01-23 19:59:34 +0100

    añado seleccion

--- EJERCICIO/src/ordenacion.java
+++ EJERCICIO/src/ordenacion.java
@@ -2,19 +2,33 @@
 public class ordenacion {
 
 
-	
-	
-	// Metodo inserccion Decreciente
-	for (int cont = 0; cont <= vector.length - 1; cont++) {
-		tmp = vector[cont];
-		vuelta = cont - 1;
+	public static void seleccion(int[] array) {
+		//Declaración de variables
+		int posicion;
+		int aux;
+		
+		//Método de la selección orden creciente
+		for(int i=0; i<=array.length-2;i++) {
+			int menor= array[i];
+			posicion=i;
+			for(int j=i+1;j<=array.length-1;j++) {
+				if(array[j]<menor) {
+					menor=array[j];
+					posicion=j;
+				}
+			}
+			if(posicion!=i) {
+				aux=array[i];
+				array[i]=array[posicion];
+				array[posicion]=aux;
+			}
+		}//for
 
-		while (vuelta >= 0 && vector[vuelta] < tmp) {
-			vector[vuelta + 1] = vector[vuelta];
-			vector[vuelta] = tmp;
-			vuelta--;
-		}
+		
+		
+		
 	}
+	
 
 	
 
