e43cf7e43237d84cb7c527a4bed678c26348ef95
David <davidramirezbustos@hotmail.com>
2018-01-23 20:35:02 +0100

    insercion completada

--- ClaseOrdenacion.java
+++ ClaseOrdenacion.java
@@ -1,72 +1,45 @@
 import java.util.Scanner;
 
 public class ClaseOrdenacion {
-	
-	public static void seleccion (int [] array) {
-		
-		int numero, menor, posicion=0, aux;
-		
-		for (int i = 0; i < 9 ; i ++) {
-        	
-        	menor = array [i];
-        	posicion = i;
-        	for (int j = i + 1; j < 10; j++ ) {
-        		
-        		if (array[j] < menor) {
-        			menor=array[j];
-        			posicion = j;
-        		}
-        		
-        	}
-        	
-        	if (posicion != i) {
-        		aux = array [i];
-        		array [i] = array[posicion];
-        		array [posicion] = aux;
-        	}
-        	
-        }
-		
-		
-	}
-	
-	
+	//Insercción
+		public static void inserccion (int[] array) {
+			//Declaración del array
+			int numero;
+			int aux;
+			int j;
+			//Proceso
+			for (int i = 0; i < 10; i++) {
+				aux = this.array[i];
+				j = i - 1;
+				while (j >= 0 && this.array[j] > aux) {
+					this.array[j + 1] = this.array[j];
+					this.array[j] = aux;
+					j--;
+				}
+			}
 
 	public static void main(String[] args) {
-		
-		int numero;
-		
-		int [] array;
+		Scanner entrada = new Scanner(System.in);
+		//Declaración del array
+		int[] array;
 		array = new int [10];
+		//Valores del array
+		for(int i = 0; i < 10; i++){
+			System.out.println("Introduce número para el array");
+			numero = entrada.nextInt();
+			array[i] = numero;
+		}
+		System.out.println("Array Original:");
+		for(int i = 0; i < 10; i++){
+			System.out.println(array[i]);			
+		}
+		ClaseOrdenacion.inserccion(array);
 		
-	
-	//Entradas
-		
-		Scanner teclado=new Scanner(System.in);
-        
-        
-        for (int i = 0; i < 10; i++) {
-        		System.out.println("Introduce un numero");
-                
-                numero = teclado.nextInt();
-                
-                array[i]=numero;
-                
-        	
-        }
-        
-        for (int i = 0; i < 10; i++) {
-    		System.out.println("Los valores del array original eran: " + array[i]);
-
-        }
-		
-		ClaseOrdenacion.seleccion(array);
-		
-		for (int i = 0; i < 10; i++) {
-    		System.out.println("Los valores del array ordenados son: " + array[i]);
-
-        }
-
+		//Salida
+		System.out.println("Array Ordenado:");
+		for(int i = 0; i < 10; i++){
+			System.out.println(array[i]);
+		}
 	}
 
 }
