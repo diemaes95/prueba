27dced1b179903fc5a47a241a8621c3b0c8bac85
ratingor <dani@email.com>
2018-01-23 20:53:45 +0100

    asdf

--- Entornos.java
+++ Entornos.java
@@ -36,4 +36,30 @@ public class Entornos {
 			}
 		}
 	}
+public static void seleccion(int[] array) {
+		int menor;
+		int posicion;
+		int aux;
+
+		for (int i = 0; i <= 10 - 2; i++) {
+			menor = array[i];
+			posicion = i;
+
+			for (int j = i + 1; j <= array.length - 1; j++) {
+
+				if (array[j] < menor) {
+					menor = array[j];
+					posicion = j;
+				}
+			}
+			if (posicion != i) {
+				aux = array[i];
+				array[i] = array[posicion];
+				array[posicion] = aux;
+
+			}
+		}
+
+	}
+
 }
