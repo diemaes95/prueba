29b0b0308c281b8c104d3617a806109095bb5337
Iván <ilopper.pio12@gmail.com>
2018-01-23 21:03:40 +0100

    cambios

--- Entornos.java
+++ Entornos.java
@@ -15,7 +15,7 @@ public class Entornos {
 		}
 	}
 
-public static void seleccion(int[] array) {
+	public static void seleccion(int[] array) {
 		int menor;
 		int posicion;
 		int aux;
@@ -39,5 +39,27 @@ public static void seleccion(int[] array) {
 			}
 		}
 
+	}
+
+	public static void insercion(int[] insercion) {
+
+		// Variables.
+
+		int i = 0;
+		int j = 0;
+		int aux = 0;
+
+		// Ordenamos el array mediante el método de inserción (crecientemente).
+
+		for (i = 0; i < insercion.length; i++) {
+			aux = insercion[i];
+			j = i - 1;
+
+			while (j >= 0 && insercion[j] > aux) {
+				insercion[j + 1] = insercion[j];
+				insercion[j] = aux;
+				j--;
+			}
+		}
 	}
 }
