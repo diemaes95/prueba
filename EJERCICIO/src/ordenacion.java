5df2fad488c58930b0aa868c1257af36a4d15316
Alumno <correo@correo.com>
2018-01-23 19:57:35 +0100

    metodo inserccion

--- EJERCICIO/src/ordenacion.java
+++ EJERCICIO/src/ordenacion.java
@@ -1,21 +1,26 @@
 
 public class ordenacion {
 
-
 	
 	
-	// Metodo inserccion Decreciente
-	for (int cont = 0; cont <= vector.length - 1; cont++) {
-		tmp = vector[cont];
-		vuelta = cont - 1;
-
-		while (vuelta >= 0 && vector[vuelta] < tmp) {
-			vector[vuelta + 1] = vector[vuelta];
-			vector[vuelta] = tmp;
-			vuelta--;
+	// Metodo inserccion Creciente
+	public void inserccion(int [] vector) {
+	
+		// Declaraciond de variables
+		int vuelta, tmp = 0;
+		
+		for (int cont = 0; cont <= vector.length - 1; cont++) {
+			tmp = vector[cont];
+			vuelta = cont - 1;
+	
+			while (vuelta >= 0 && vector[vuelta] > tmp) {
+				vector[vuelta + 1] = vector[vuelta];
+				vector[vuelta] = tmp;
+				vuelta--;
+			}
 		}
 	}
 
 	
-
+	
 }
