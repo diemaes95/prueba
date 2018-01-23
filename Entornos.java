8da730c15674c96324bf616c0a71c4a33f1647da
ratingor <dani@email.com>
2018-01-23 20:59:09 +0100

    asd

--- Entornos.java
+++ Entornos.java
@@ -15,27 +15,6 @@ public class Entornos {
 		}
 	}
 
-	public static void insercion(int[] insercion) {
-
-		// Variables.
-
-		int i = 0;
-		int j = 0;
-		int aux = 0;
-
-		// Ordenamos el array mediante el método de inserción (crecientemente).
-
-		for (i = 0; i < insercion.length; i++) {
-			aux = insercion[i];
-			j = i - 1;
-
-			while (j >= 0 && insercion[j] > aux) {
-				insercion[j + 1] = insercion[j];
-				insercion[j] = aux;
-				j--;
-			}
-		}
-	}
 public static void seleccion(int[] array) {
 		int menor;
 		int posicion;
@@ -61,5 +40,4 @@ public static void seleccion(int[] array) {
 		}
 
 	}
-
 }
