e82077c021610090dba70c441d3276608e881cec
Ruben <rubentijeras44@gmail.com>
2018-01-23 20:20:34 +0100

    patata

--- git/src/git/ordenacion.java
+++ git/src/git/ordenacion.java
@@ -1,40 +1,34 @@
 package git;
 
 public class ordenacion {
-	
-<<<<<<< HEAD
-	public static void insercion (int[] vector) {
 
-	int aux;	
-        for(int i=0;i<10-1;i++) {
-        	aux=vector[i];
-        	int j=i-1;
-        	while(j>=0 && vector[j]>aux){
-        		vector[j+1]=vector[j];
-        		vector[j]=aux;
-        		j--;
-        	}
-        }
-=======
+	public static void insercion(int[] vector) {
 
-		
-		
-		public static void burbuja (int[] vector) {
-			int aux;
-			for (int i = 0; i < 10; i++) {
-				aux = vector[i];
-				int j = i - 1;
-
-				while (j >= 0 && vector[j] < aux) {
-					vector[j+1] = vector[j];
-					vector[j] = aux;
-					j--;
-				}
+		int aux;
+		for (int i = 0; i < 10 - 1; i++) {
+			aux = vector[i];
+			int j = i - 1;
+			while (j >= 0 && vector[j] > aux) {
+				vector[j + 1] = vector[j];
+				vector[j] = aux;
+				j--;
 			}
-			
-		
->>>>>>> 3e50dccf8dffef02a2f9b27c44bca7169ce148be
-		
+		}
+	}
+
+	public static void burbuja(int[] vector) {
+		int aux;
+		for (int i = 0; i < 10; i++) {
+			aux = vector[i];
+			int j = i - 1;
+
+			while (j >= 0 && vector[j] < aux) {
+				vector[j + 1] = vector[j];
+				vector[j] = aux;
+				j--;
+			}
+		}
+
 	}
 
 }
