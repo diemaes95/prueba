3e50dccf8dffef02a2f9b27c44bca7169ce148be
Ruben <rubentijeras44@gmail.com>
2018-01-23 20:02:18 +0100

    patata

--- git/src/git/ordenacion.java
+++ git/src/git/ordenacion.java
@@ -2,7 +2,22 @@ package git;
 
 public class ordenacion {
 	
-	public static void insercion (int[] vector) {
+
+		
+		
+		public static void burbuja (int[] vector) {
+			int aux;
+			for (int i = 0; i < 10; i++) {
+				aux = vector[i];
+				int j = i - 1;
+
+				while (j >= 0 && vector[j] < aux) {
+					vector[j+1] = vector[j];
+					vector[j] = aux;
+					j--;
+				}
+			}
+			
 		
 		
 	}
