dc986d20e1f787bac8605bf486ddb91bbacbb4d8
Pablo <pablorodricp@gmail.com>
2018-01-23 19:52:35 +0100

    Metodo de insercion anadido

--- ordenacion.java
+++ ordenacion.java
@@ -1,5 +1,19 @@
 
 public class ordenacion {
+	
+	public static void Insercion(int[] vector) {
 
+		int j;
+		int aux;
+		for (int i = 0; i <= vector.length - 1; i++) {
+			aux = vector[i];
+			j = i - 1;
+			while (j >= 0 && vector[j] > aux) {
+				vector[j + 1] = vector[j];
+				vector[j] = aux;
+				j--;
+			}
+		}
+	}
 	
 }
