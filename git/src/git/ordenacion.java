cd958780e2acea98afdf83ac1cf17da1ca51d929
juanra <aaa@hotmail.com>
2018-01-23 20:09:54 +0100

    mamada

--- git/src/git/ordenacion.java
+++ git/src/git/ordenacion.java
@@ -3,7 +3,17 @@ package git;
 public class ordenacion {
 	
 	public static void insercion (int[] vector) {
-		
+
+	int aux;	
+        for(int i=0;i<10-1;i++) {
+        	aux=vector[i];
+        	int j=i-1;
+        	while(j>=0 && vector[j]>aux){
+        		vector[j+1]=vector[j];
+        		vector[j]=aux;
+        		j--;
+        	}
+        }
 		
 	}
 
