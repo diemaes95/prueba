bcb0f81dbe572ca9774c06feb7ce9096ddefa4e7
Iván <ilopper.pio12@gmail.com>
2018-01-23 21:09:27 +0100

    insercion

--- Entornos.java
+++ Entornos.java
@@ -1,45 +1,7 @@
 
 public class Entornos {
 
-	public static void Burbuja(int[] array) {
-
-		for (int i = 0; i < 10 - 1; i++) {
-			for (int j = 0; j < 10 - i - 1; j++) {
-
-				if (array[j + 1] < array[j]) {
-					int aux = array[j + 1];
-					array[j + 1] = array[j];
-					array[j] = aux;
-				}
-			}
-		}
-	}
-
-	public static void seleccion(int[] array) {
-		int menor;
-		int posicion;
-		int aux;
-
-		for (int i = 0; i <= 10 - 2; i++) {
-			menor = array[i];
-			posicion = i;
-
-			for (int j = i + 1; j <= array.length - 1; j++) {
-
-				if (array[j] < menor) {
-					menor = array[j];
-					posicion = j;
-				}
-			}
-			if (posicion != i) {
-				aux = array[i];
-				array[i] = array[posicion];
-				array[posicion] = aux;
-
-			}
-		}
-
-	}
+	
 
 	public static void insercion(int[] insercion) {
 
