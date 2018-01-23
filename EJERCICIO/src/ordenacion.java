a163497238eb23fb7413db491ad0ca2d5f34b99b
lujan <mlujanj@outlook.es>
2018-01-23 20:00:51 +0100

    añado burbuja desc

--- EJERCICIO/src/ordenacion.java
+++ EJERCICIO/src/ordenacion.java
@@ -3,19 +3,16 @@ public class ordenacion {
 
 
 	
-	
-	// Metodo inserccion Decreciente
-	for (int cont = 0; cont <= vector.length - 1; cont++) {
-		tmp = vector[cont];
-		vuelta = cont - 1;
-
-		while (vuelta >= 0 && vector[vuelta] < tmp) {
-			vector[vuelta + 1] = vector[vuelta];
-			vector[vuelta] = tmp;
-			vuelta--;
+	public static void burbujaDesc(int[] array) {
+	for (int i = 0; i < array.length - 1; i++) {
+		for (int j = 0; j <= array.length - i - 2; j++)
+			if (array[j + 1] > array[j]) {
+				tmp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = tmp;
+			}
 		}
 	}
 
-	
 
 }
