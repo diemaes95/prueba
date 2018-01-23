b8384ab4d3e7f5cc2ba5179b6ab1b83a34ab26e8
Iván <ilopper.pio12@gmail.com>
2018-01-23 20:12:30 +0100

    subo entornos

---
+++ Entornos.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Entornos {
+
+	public static void insercion(int[] insercion) {
+
+		// Variables.
+
+		int i = 0;
+		int j = 0;
+		int aux = 0;
+
+		// Ordenamos el array mediante el método de inserción (crecientemente).
+
+		for (i = 0; i < insercion.length; i++) {
+			aux = insercion[i];
+			j = i - 1;
+
+			while (j >= 0 && insercion[j] > aux) {
+				insercion[j + 1] = insercion[j];
+				insercion[j] = aux;
+				j--;
+			}
+		}
+	}
+}
