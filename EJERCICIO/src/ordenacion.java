ed1242d0df8d90b491ef4c8ea970637f71b7a9d9
alumno <correo@correo.com>
2018-01-23 20:06:50 +0100

    cambio

--- EJERCICIO/src/ordenacion.java
+++ EJERCICIO/src/ordenacion.java
@@ -1,18 +1,32 @@
 
 public class ordenacion {
 
-	
-	
+	// Metodo burbuja ascendente
+	public static void burbuja(int[] elementos) {
+		int aux, i, j;
+		int[] elementos = new int[10];
+
+		for (i = 0; i <= 10; i++) {
+			for (j = 0; j <= 10 - i - 2; j++) {
+				if (elementos[j + 1] < elementos[j]) {
+					aux = elementos[j + 1];
+					elementos[j + 1] = elementos[j];
+					elementos[j] = aux;
+				}
+			}
+		}
+	}
+
 	// Metodo inserccion Creciente
-	public void inserccion(int [] vector) {
-	
+	public void inserccion(int[] vector) {
+
 		// Declaraciond de variables
 		int vuelta, tmp = 0;
-		
+
 		for (int cont = 0; cont <= vector.length - 1; cont++) {
 			tmp = vector[cont];
 			vuelta = cont - 1;
-	
+
 			while (vuelta >= 0 && vector[vuelta] > tmp) {
 				vector[vuelta + 1] = vector[vuelta];
 				vector[vuelta] = tmp;
@@ -21,6 +35,4 @@ public class ordenacion {
 		}
 	}
 
-	
-	
 }
