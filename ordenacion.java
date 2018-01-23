856932b7b890b697a7fba7658b5dde356d343c69
David <errs@hotmail.es>
2018-01-23 19:57:53 +0100

    subo metodo burbuja

--- ordenacion.java
+++ ordenacion.java
@@ -1,5 +1,25 @@
 
 public class ordenacion {
 
-	
+	public static void burbuja(int[] vector1) {
+
+		// Variables
+
+		int aux = 0;
+
+		// Ordenar vector
+
+		for (int i = 0; i < vector1.length - 1; i++) {
+
+			for (int j = 0; j < vector1.length - i - 1; j++) {
+
+				if (vector1[j + 1] < vector1[j]) {
+
+					aux = vector1[j + 1];
+					vector1[j + 1] = vector1[j];
+					vector1[j] = aux;
+				}
+			}
+		}
+	}
 }
