e3e665054f506084a523911a54833a6b933d8afc
Antonio <antor13ramon@hotmail.com>
2018-01-23 20:24:41 +0100

    ActualizacionBurbuja

--- PutosAmos/src/Ordenacion.java
+++ PutosAmos/src/Ordenacion.java
@@ -3,14 +3,14 @@ public class Ordenacion {
 	
 			// variables//
 
-			private int numero, i, j, temp;
+			private int numero, i, j, temp, aux;
 
 			// vector//
 
 			private int[] vector;
 			vector = new private int[10];
 	
-	public static void InsercionAscendente (vector int[]) {
+	public static void insercionAscendente (int [] vector) {
 		for (i = 0; i < vector.length; i++) {
 			temp = vector[i];
 			j = i - 1;
@@ -22,7 +22,7 @@ public class Ordenacion {
 		}
 	}
 	
-	public static void InsercionDescendente (vector int[]) {
+	public static void insercionDescendente (int [] vector) {
 		for (i = 0; i < vector.length; i++) {
 			temp = vector[i];
 			j = i - 1;
@@ -32,6 +32,22 @@ public class Ordenacion {
 				j--;
 			}
 		}
+	}
+	
+	public static void burbuja(int [] vector) { // Ascendente
+		
+		for (int i = 0; i < vector.length - 1; i++) // Ordenación del vector
+
+			for (int j = 0; j < vector.length - 1 - i; j++) {
+
+				if (vector[j] > vector[j + 1]) { // Si hay desorden entre dos consecutivos, estos se cambian
+
+					aux = vector[j];
+					vector[j] = vector[j + 1];
+					vector[j + 1] = aux;
+				}
+
+			}
 	}
 
 	
