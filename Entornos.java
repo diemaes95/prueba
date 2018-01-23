517aa6a18a81d0469245d8507349efa5f59d66b0
unknown <danimore_16@hotmail.com>
2018-01-23 20:48:06 +0100

    entornos

---
+++ Entornos.java
@@ -0,0 +1,53 @@
+
+
+public class Entornos{
+
+	public static void Burbuja(int[] array) {
+
+	
+		for (int i = 0; i < 10 - 1; i++) {
+			for (int j = 0; j < 10 - i - 1; j++) {
+				
+				if(array[j + 1] < array[j]) {
+					int aux = array[j + 1];
+					array[j + 1] = array[j];
+					array[j] = aux;
+				}
+			}
+		}
+	}
+}
+		
+	
+			
+			
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
