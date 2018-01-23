407e27d4f4180160c5d59a8e19b0cc813c695c9a
DanielGC <hola@hola.com>
2018-01-23 20:12:27 +0100

    seleccion

--- EJERCICIO/src/ordenacion.java
+++ EJERCICIO/src/ordenacion.java
@@ -1,10 +1,32 @@
 
 public class ordenacion {
 
+	public static void seleccion(int[] array) {
+		int aux, posicion;
+		
+		//Método de la selección orden creciente
+		for(int i=0; i<=array.length-2;i++) {
+			int menor= array[i];
+			posicion=i;
+			for(int j=i+1;j<=array.length-1;j++) {
+				if(array[j]<menor) {
+					menor=array[j];
+					posicion=j;
+				}
+			}
+			if(posicion!=i) {
+				aux=array[i];
+				array[i]=array[posicion];
+				array[posicion]=aux;
+			}
+		}//for
+	}
+	
+	
 	// Metodo burbuja ascendente
 	public static void burbuja(int[] elementos) {
 		int aux, i, j;
-		int[] elementos = new int[10];
+		
 
 		for (i = 0; i <= 10; i++) {
 			for (j = 0; j <= 10 - i - 2; j++) {
