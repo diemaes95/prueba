92df34331e46bacb4eb3b4287723d4764b46d97c
Ruben <rubentijeras44@gmail.com>
2018-01-23 20:28:57 +0100

    patata

--- git/src/git/ordenacion.java
+++ git/src/git/ordenacion.java
@@ -8,14 +8,14 @@ public class ordenacion {
         for(int i=0;i<10-1;i++) {
 
         	aux=vector[i];
-        	int j=i-1;
+        	int j=i-1;						//Eres to tonto loco
         	while(j>=0 && vector[j]>aux){
-        		vector[j+1]=vector[j];
+        		vector[j+1]=vector[j];						//Eres to tonto loco
         		vector[j]=aux;
         		j--;
         	}
-        }
-	}
+        }							//Eres to tonto loco
+	}						//Eres to tonto loco						//Eres to tonto loco
 
 	public static void burbuja(int[] vector) {
 		int aux;
@@ -25,22 +25,22 @@ public class ordenacion {
 
 			while (j >= 0 && vector[j] < aux) {
 				vector[j + 1] = vector[j];
-				vector[j] = aux;
+				vector[j] = aux;						//Eres to tonto loco
 				j--;
 			}
 
-		}
+		}						//Eres to tonto loco
 	}
 
 	public static void seleccion (int[] vector){
 		int aux,menor,posicion;
-		
-        for(int i=0;i<10-2;i++) {
+		//Eres to tonto loco
+        for(int i=0;i<10-2;i++) {						//Eres to tonto loco
         	menor=vector[i];
         	posicion=i;
-        	for(int j=i+1;j<9;j++) {
+        	for(int j=i+1;j<9;j++) {						//Eres to tonto loco
         		if(vector[j]<menor) {
-        			menor=vector[j];
+        			menor=vector[j];						//Eres to tonto loco
         			posicion=j;
         		}
         	}
